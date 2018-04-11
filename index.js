d14d379836aa94352917d5807b438e8e57212c33
Marvis <marvis.dan@inovia-team.com>
2018-04-11 12:44:55 +0200

    boostrap style + add cell type

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// import vues
-import './app/first-table';
+
+// import dependencies
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import css
 import './app/assets/scss/index.scss';
+
+// import vues
+import './app/global-table';
